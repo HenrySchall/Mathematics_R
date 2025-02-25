57537050c61fa1d28ad2da435a07131e53bb9d8d
Henrique Werner <henrique.schall@outlook.com>
2025-02-25 13:18:45 -0300

    .

---
+++ R/1.Statistics.r
@@ -0,0 +1,16 @@
+library(magrittr)
+library(rio)      
+library(timetk)   
+library(dplyr)    
+library(tidyr)    
+library(ggplot2) 
+library(readr)
+library(readxl)
+library(forecast)
+library(zoo)
+library(rio) 
+library(timetk)
+
+#################
+## Estatística ##
+#################
